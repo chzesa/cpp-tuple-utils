d14a5c6377095f1595503607fbc8db105ec3d425
chzesa <chzesa+git@gmail.com>
2025-12-30 08:12:52 +0200

    Rewrite variant logic

--- variant.hpp
+++ variant.hpp
@@ -3,33 +3,36 @@
 #include <utility>
 #include <cstddef>
 #include <tuple>
+#include <cstring>
 
 namespace tuple_utils
 {
 
-template <typename Value, typename ...Rest>
+template <typename T>
+static constexpr T max(T a, T b)
+{
+	return a > b ? a : b;
+}
+
+template <size_t N, typename Value, typename ...Rest>
 struct VariantImpl
 {
-	template <typename T>
-	static constexpr T max(T a, T b)
-	{
-		return a > b ? a : b;
-	}
+	using Next = VariantImpl<N, Rest...>;
 
 	static constexpr size_t alignOf()
 	{
-		return max(alignof(Value), VariantImpl<Rest...>::alignOf());
+		return max(alignof(Value), Next::alignOf());
 	}
 
 	template <typename V>
 	static constexpr size_t keyOf()
 	{
-		return std::is_same<V, Value>() ? sizeof...(Rest) : VariantImpl<Rest...>::template keyOf<V>();
+		return std::is_same<V, Value>() ? N - sizeof...(Rest) : Next::template keyOf<V>();
 	}
 
 	static constexpr size_t sizeOf()
 	{
-		return max(sizeof(Value), VariantImpl<Rest...>::sizeOf());
+		return max(sizeof(Value), Next::sizeOf());
 	}
 
 	static void destroy(const size_t& i, void* p)
@@ -37,7 +40,7 @@ struct VariantImpl
 		if (i == keyOf<Value>())
 			reinterpret_cast<Value*>(p)->~Value();
 		else
-			VariantImpl<Rest...>::destroy(i, p);
+			Next::destroy(i, p);
 	}
 
 	template <typename T, typename ...Params>
@@ -46,21 +49,22 @@ struct VariantImpl
 		if (keyOf<Value>() == key)
 			T::template callback<Value>(std::forward<Params>(params)...);
 		else
-			VariantImpl<Rest...>::template switchf<T>(key, std::forward<Params>(params)...);
+			Next::template switchf<T>(key, std::forward<Params>(params)...);
 	}
 
 	template <typename F, typename T>
-	void switchf(const size_t& key, F&& f, T& t)
+	static void switchf2(const size_t& key, F f, T& t)
 	{
 		if (keyOf<Value>() == key)
-			f(t.template get<T>());
+			f(t.template get<Value>());
 		else
-			VariantImpl<Rest...>::template switchf<T>(std::forward(f), t);
+			Next::template switchf2(key, f, t);
 	}
 };
 
-template <typename Value>
-struct VariantImpl <Value>
+
+template <size_t N, typename Value>
+struct VariantImpl <N, Value>
 {
 	static constexpr size_t alignOf()
 	{
@@ -70,7 +74,7 @@ struct VariantImpl <Value>
 	template <typename V>
 	static constexpr size_t keyOf()
 	{
-		return std::is_same<V, Value>() ? 0 : -1;
+		return std::is_same<V, Value>() ? N : -1;
 	}
 
 	static constexpr size_t sizeOf()
@@ -92,16 +96,20 @@ struct VariantImpl <Value>
 	}
 
 	template <typename F, typename T>
-	void switchf(const size_t& key, F&& f, T& t)
+	static void switchf2(const size_t& key, F f, T& t)
 	{
 		if (keyOf<Value>() == key)
-			f(t.template get<T>());
+			f(t.template get<Value>());
 	}
 };
 
+
 template <typename ...Values>
-struct alignas(VariantImpl<Values...>::alignOf() * sizeof(size_t)) Variant
+struct alignas(VariantImpl<sizeof...(Values), Values...>::alignOf() * sizeof(size_t)) Variant
 {
+	using tuple = std::tuple<Values...>;
+	using Impl = VariantImpl<sizeof...(Values) - 1, Values...>;
+
 	static constexpr size_t size()
 	{
 		return sizeof...(Values);
@@ -112,12 +120,34 @@ struct alignas(VariantImpl<Values...>::alignOf() * sizeof(size_t)) Variant
 		_key = -1;
 	}
 
+	Variant(Variant&& v)
+	{
+		_key = v._key;
+		memcpy(data, v.data, sizeof data);
+	}
+
+	Variant(const Variant& v)
+	{
+		_key = v._key;
+		switchf2([&] <typename T> (T& value) {
+			value = v.template view<T>();
+		});
+	}
+
+	Variant(Variant& v)
+	{
+		_key = v._key;
+		switchf2([&] <typename T> (T& value) {
+			value = v.template view<T>();
+		});
+	}
+
 	template <typename V>
 	explicit Variant(const V& v)
 	{
 		static_assert(keyOf<V>() != -1, "Type is not a member of variant.");
 		_key = keyOf<V>();
-		*(reinterpret_cast<V*>(data)) = v;
+		new(data) V(v);
 	}
 
 	template <typename V>
@@ -125,24 +155,53 @@ struct alignas(VariantImpl<Values...>::alignOf() * sizeof(size_t)) Variant
 	{
 		static_assert(keyOf<V>() != -1, "Type is not a member of variant.");
 		_key = keyOf<V>();
-		*(reinterpret_cast<typename std::remove_reference<V>::type*>(data)) = v;
+		new (data) std::remove_reference<V>::type(v);
+	}
+
+	template <typename V>
+	Variant& operator = (const V& v)
+	{
+		static_assert(keyOf<V>() != -1, "Type is not a member of variant.");
+		_key = keyOf<V>();
+		new(data) V(v);
+		return *this;
+	}
+
+	Variant& operator = (const Variant& v)
+	{
+		_key = v._key;
+		switchf2([&] <typename T> (T& value) {
+			value = v.template view<T>();
+		});
+		return *this;
 	}
 
 	~Variant()
 	{
-		VariantImpl<Values...>::destroy(_key, data);
+		Impl::destroy(_key, data);
 	}
 
 	template <typename F>
-	void switchf(F&& f)
+	bool switchf2(F f)
 	{
-		VariantImpl<Values...>::template switchf(_key, f, *this);
+		Impl::template switchf2(_key, f, *this);
+		return _key != -1;
 	}
 
 	template <typename T, typename ...Params>
 	void switchf(Params&&... params)
 	{
-		VariantImpl<Values...>::template switchf<T>(_key, std::forward<Params>(params)...);
+		Impl::template switchf<T>(_key, std::forward<Params>(params)...);
+	}
+
+	template <typename V>
+	static Variant construct()
+	{
+		static_assert(keyOf<V>() != -1, "Type is not a member of variant.");
+		Variant ret;
+		ret._key = keyOf<V>();
+		new (reinterpret_cast<V*>(ret.data)) V();
+		return ret;
 	}
 
 	template <typename V, typename ...Params>
@@ -155,10 +214,20 @@ struct alignas(VariantImpl<Values...>::alignOf() * sizeof(size_t)) Variant
 		return ret;
 	}
 
+	static Variant fromKey(const size_t& i)
+	{
+		Variant ret;
+		ret._key = i;
+		ret.switchf2([] <typename T> (T& t) {
+			new (&t) T();
+		});
+		return ret;
+	}
+
 	template <typename V>
 	static constexpr size_t keyOf()
 	{
-		return VariantImpl<Values...>::template keyOf<typename std::remove_reference<V>::type>();
+		return Impl::template keyOf<typename std::remove_reference<V>::type>();
 	}
 
 	template <typename V>
@@ -179,7 +248,7 @@ struct alignas(VariantImpl<Values...>::alignOf() * sizeof(size_t)) Variant
 	void set(const V& v)
 	{
 		static_assert(keyOf<V>() != -1, "Type is not a member of variant.");
-		if (key() != -1) VariantImpl<Values...>::destroy(_key, data);
+		if (key() != -1) Impl::destroy(_key, data);
 		_key = keyOf<V>();
 		*(reinterpret_cast<V*>(data)) = v;
 	}
@@ -188,7 +257,7 @@ struct alignas(VariantImpl<Values...>::alignOf() * sizeof(size_t)) Variant
 	void set(V&& v)
 	{
 		static_assert(keyOf<V>() != -1, "Type is not a member of variant.");
-		if (key() != -1) VariantImpl<Values...>::destroy(_key, data);
+		if (key() != -1) Impl::destroy(_key, data);
 		_key = keyOf<V>();
 		*(reinterpret_cast<typename std::remove_reference<V>::type*>(data)) = v;
 	}
@@ -198,9 +267,25 @@ struct alignas(VariantImpl<Values...>::alignOf() * sizeof(size_t)) Variant
 		return _key;
 	}
 
+	template <typename V>
+	size_t keyOf2() const
+	{
+		static_assert(keyOf<V>() != -1, "Type is not a member of variant.");
+		return Variant::keyOf<V>();
+	}
+
 private:
-	char data[VariantImpl<Values...>::sizeOf()];
+	char data[Impl::sizeOf()];
 	size_t _key;
+};
+
+template <typename ...Values>
+struct variant_from_tuple;
+
+template <typename ...Values>
+struct variant_from_tuple<std::tuple<Values...>>
+{
+	using type = Variant<Values...>;
 };
 
 } // namespace tuple_utils
